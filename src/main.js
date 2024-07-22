a70ceb6b07786e5170b421124c4c5083809e2944
ruudra03 <rudra03.174@gmail.com>
2024-07-22 17:24:10 +0530

    App Setup

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vue router
+import router from './router'
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="home">
+        <h1>Dashboard</h1>
+    </main>
+</template>
